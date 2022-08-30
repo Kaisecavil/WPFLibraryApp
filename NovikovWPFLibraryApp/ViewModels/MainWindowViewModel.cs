37f89eaf767ac282add1d1de3fe113f6b21ffa40
Kaisecavil <combatzara5094@mail.ru>
2022-08-30 16:36:53 +0300

    view model

---
+++ NovikovWPFLibraryApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using NovikovWPFLibraryApp.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NovikovWPFLibraryApp.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+		private string _Title;
+
+		public string Title
+		{
+			get { return _Title; }
+			set
+			{
+				Set(ref _Title, value);
+			}
+		}
+
+	}
+}
